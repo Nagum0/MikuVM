b7c1999748785a69ec6b88d30c7150dbb770115a
Nagum0 <zackbene9@gmail.com>
2024-10-27 15:52:03 +0100

    Types finished and tested. Also updated README.

--- src/lib/types.rs
+++ src/lib/types.rs
@@ -1,6 +1,6 @@
 use crate::{error::MikuError, tools};
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum MikuType {
     U8(u8),
     U16(u16),
@@ -10,6 +10,8 @@ pub enum MikuType {
     I16(i16),
     I32(i32),
     I64(i64),
+    F32(f32),
+    F64(f64),
 }
 
 /// Macro for implementing operations for MikuType.
@@ -18,7 +20,8 @@ macro_rules! implement_operation {
         pub fn $method_name(a: MikuType, b: MikuType) -> Result<MikuType, MikuError> {
             match (stringify!($op), &b) {
                 ("/", MikuType::U8(0) | MikuType::U16(0) | MikuType::U32(0) | MikuType::U64(0) |
-                      MikuType::I8(0) | MikuType::I16(0) | MikuType::I32(0) | MikuType::I64(0)) => {
+                      MikuType::I8(0) | MikuType::I16(0) | MikuType::I32(0) | MikuType::I64(0) | 
+                      MikuType::F32(0.0) | MikuType::F64(0.0)) => {
                         return Err(MikuError::DivisionByZeroError);
                 }
                 _ => {},
@@ -33,6 +36,8 @@ macro_rules! implement_operation {
                 (Self::I16(a), Self::I16(b)) => Ok(Self::I16(a $op b)),
                 (Self::I32(a), Self::I32(b)) => Ok(Self::I32(a $op b)),
                 (Self::I64(a), Self::I64(b)) => Ok(Self::I64(a $op b)),
+                (Self::F32(a), Self::F32(b)) => Ok(Self::F32(a $op b)),
+                (Self::F64(a), Self::F64(b)) => Ok(Self::F64(a $op b)),
                 _ => Err(MikuError::UndefinedOperationBetweenTypesError(format!("{:?} {} {:?}", a, stringify!($op), b))),
             }
         }
@@ -75,8 +80,9 @@ impl From<MikuType> for Vec<u8> {
 	fn from(value: MikuType) -> Self {
 		match_to_bytes!(
             value, { U8 => 0x00, U16 => 0x01, U32 => 0x02, U64 => 0x03,
-                     I8 => 0x04, I16 => 0x05, I32 => 0x06, I64 => 0x07
-        })
+                     I8 => 0x04, I16 => 0x05, I32 => 0x06, I64 => 0x07,
+                     F32 => 0x08, F64 => 0x09 }
+        )
 	}
 }
 
@@ -91,7 +97,8 @@ impl TryFrom<&[u8]> for MikuType {
         match_from_bytes!(
             type_identifier_byte, le_bytes, 
             { 0x00 => U8 | u8, 0x01 => U16 | u16, 0x02 => U32 | u32, 0x03 => U64 | u64,
-              0x04 => I8 | i8, 0x05 => I16 | i16, 0x06 => I32 | i32, 0x07 => I64 | i64
+              0x04 => I8 | i8, 0x05 => I16 | i16, 0x06 => I32 | i32, 0x07 => I64 | i64,
+              0x08 => F32 | f32, 0x09 => F64 | f64
         })
 	}
 }
@@ -104,14 +111,16 @@ impl MikuType {
 
     pub fn eq(a: MikuType, b: MikuType) -> Result<bool, MikuError> {
         match (a, b) {
-            (Self::U8(a), Self::U8(b))     => Ok(a == b),
+            (Self::U8(a), Self::U8(b))   => Ok(a == b),
             (Self::U16(a), Self::U16(b)) => Ok(a == b),
             (Self::U32(a), Self::U32(b)) => Ok(a == b),
             (Self::U64(a), Self::U64(b)) => Ok(a == b),
-            (Self::I8(a), Self::I8(b))     => Ok(a == b),
+            (Self::I8(a), Self::I8(b))   => Ok(a == b),
             (Self::I16(a), Self::I16(b)) => Ok(a == b),
             (Self::I32(a), Self::I32(b)) => Ok(a == b),
             (Self::I64(a), Self::I64(b)) => Ok(a == b),
+            (Self::F32(a), Self::F32(b)) => Ok(a == b),
+            (Self::F64(a), Self::F64(b)) => Ok(a == b),
             _ => Err(MikuError::UndefinedOperationBetweenTypesError(format!("Cannot check equality between {:?} and {:?}", a, b))),
         }
     }
